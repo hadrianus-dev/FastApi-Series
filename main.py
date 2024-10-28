8511a65217223ceb68792b6d248165a9ddfd1fec
David Hadrianus <95863744+hadrianus-dev@users.noreply.github.com>
2024-10-28 13:59:13 +0100

    config: settings basics information of application

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 from fastapi import FastAPI
 from src.books.routes import book_router
 
-version = 'v1'
+version = "v1"
 
 app = FastAPI(
-    version = version
+    title="Bookly API",
+    description="A Rest API for book review webservice",
+    version=version,
 )
 
 # register route
-app.include_router(
-    router = book_router,
-    prefix = f'/api/{version}/books'
-)
+app.include_router(router=book_router, prefix=f"/api/{version}/books", tags=["books"])
