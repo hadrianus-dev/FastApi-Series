e150238c69b7f96d0ccf4ee6b542cca7360c410c
David Hadrianus <95863744+hadrianus-dev@users.noreply.github.com>
2024-10-28 13:33:41 +0100

    feature: isolating schemas and book's data in memory

---
+++ src/books/data.py
@@ -0,0 +1,39 @@
+
+books = [
+    {
+        'id': 1,
+        'title': 'Think Python',
+        'author': 'Allen B Downey',
+        'publisher': "O'Reilly Media",
+        'page_count': 1234,
+        'published_date': '2001-01-01',
+        'language': 'English'
+    },
+    {
+        'id': 2,
+        'title': 'Django By Example',
+        'author': 'Antonio Balle',
+        'publisher': "Packt Publishing Ltd",
+        'page_count': 1023,
+        'published_date': '2002-01-29',
+        'language': 'English'
+    },
+    {
+        'id': 3,
+        'title': 'The Web Socket HandBook',
+        'author': 'Alex Diacomu',
+        'publisher': "Xinyu Wang",
+        'page_count': 3677,
+        'published_date': '2001-01-01',
+        'language': 'English'
+    },
+    {
+        'id': 4,
+        'title': 'Read First Javascript',
+        'author': 'Hellen Smith',
+        'publisher': "Oreilly Media",
+        'page_count': 540,
+        'published_date': '2001-01-01',
+        'language': 'English'
+    },
+]
